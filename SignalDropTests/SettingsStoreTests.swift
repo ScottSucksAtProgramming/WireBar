8e64cd14f88345c5066e073cab86a33f091f2b15
ScottSucksAtProgramming <ScottKostolni+github@gmail.com>
2026-06-22 21:14:28 -0400

    feat: add network detail visibility settings to SettingsStore

--- SignalDropTests/SettingsStoreTests.swift
+++ SignalDropTests/SettingsStoreTests.swift
@@ -41,4 +41,47 @@ final class SettingsStoreTests: XCTestCase {
         let store2 = SettingsStore(defaults: testDefaults)
         XCTAssertFalse(store2.showNetworkName)
     }
+
+    func testDefaultShowBandIsFalse() {
+        let store = SettingsStore(defaults: testDefaults)
+        XCTAssertFalse(store.showBand)
+    }
+
+    func testDefaultShowChannelIsFalse() {
+        let store = SettingsStore(defaults: testDefaults)
+        XCTAssertFalse(store.showChannel)
+    }
+
+    func testDefaultShowLinkSpeedIsFalse() {
+        let store = SettingsStore(defaults: testDefaults)
+        XCTAssertFalse(store.showLinkSpeed)
+    }
+
+    func testDefaultShowBSSIDIsFalse() {
+        let store = SettingsStore(defaults: testDefaults)
+        XCTAssertFalse(store.showBSSID)
+    }
+
+    func testDefaultShowDNSIsFalse() {
+        let store = SettingsStore(defaults: testDefaults)
+        XCTAssertFalse(store.showDNS)
+    }
+
+    func testDefaultShowGatewayIsFalse() {
+        let store = SettingsStore(defaults: testDefaults)
+        XCTAssertFalse(store.showGateway)
+    }
+
+    func testDefaultShowSubnetIsFalse() {
+        let store = SettingsStore(defaults: testDefaults)
+        XCTAssertFalse(store.showSubnet)
+    }
+
+    func testSetShowBandPersists() {
+        let store = SettingsStore(defaults: testDefaults)
+        store.showBand = true
+
+        let store2 = SettingsStore(defaults: testDefaults)
+        XCTAssertTrue(store2.showBand)
+    }
 }
